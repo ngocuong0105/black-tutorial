070a54aaca99028abdd08ebcafc1fd03483dbd20
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-21 13:33:57 +0200

    unformatted file

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+# magic traling comma
+TRANSLATIONS = {
+    "en_us": 
+    "English (US)",
+    "pl_pl": "polski",
+}
+
+# no trailing comma
+TRANSLATIONS = {"en_us": 
+"English (US)", 
+"pl_pl": 
+"polski"}
